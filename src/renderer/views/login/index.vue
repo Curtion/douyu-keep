f18a9cf4d148ae01bc5b3da5d6f53912e64347b1
Curtion <curtion@126.com>
2023-12-16 22:53:43 +0800

    chore: 首次进入软件时首页提示修改

--- src/renderer/views/login/index.vue
+++ src/renderer/views/login/index.vue
@@ -53,8 +53,8 @@ checkLogin()
     <button v-if="!loading" class="text-black hover:underline" @click="dialog = true">
       登录斗鱼账号
     </button>
-    <div class="text-black text-sm">
-      请稍等, 重新登录中...
+    <div v-else class="text-black text-sm">
+      请稍等, 校验登录状态中...
     </div>
   </div>
   <div class="text-center">
